8be4eb3398695a4b6384b1840847ad42de3055e3
Tomcariello <tomcariello@gmail.com>
2018-04-04 17:06:21 -0400

    made the start menu button functional

---
+++ Assets/scripts/menuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class menuController : MonoBehaviour {
+
+	//Get link to button
+	[SerializeField]
+	private Button startGameButton;
+
+	// Use this for initialization
+	void Start () {
+        startGameButton.onClick.AddListener(StartGameClick);
+	}
+	
+    void StartGameClick() {
+        Debug.Log("You have clicked the Start Game Button!");
+		SceneManager.LoadScene("Level_1");
+    }
+}
